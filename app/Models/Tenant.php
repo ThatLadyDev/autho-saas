3d8ce689ebf051df207e881a55542ec769b08eec
Lois Onyinyemme Bassey <loisbassey@gmail.com>
2024-10-21 23:37:16 +0100

    added information to the tenants table model class

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tenant extends Model
 {
-    //
+    protected $fillable = ['name'];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }
